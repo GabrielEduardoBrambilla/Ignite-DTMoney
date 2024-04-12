5e5101aac6f58d2129ac9b27e500f642c8459b62
Brambs <gabriel128.br@gmail.com>
2024-04-12 10:35:44 -0300

    Summary component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,30 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyBtc } from 'phosphor-react'
+import { SummaryContainer, SummaryCard } from './styles'
+
+export default function Sumarry() {
+  return (
+    <SummaryContainer>
+      <SummaryCard className="">
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+      <SummaryCard className="">
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+      <SummaryCard className="" variant="green">
+        <header>
+          <span>Entradas</span>
+          <CurrencyBtc size={32} color="#FFF" />
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
+}
