661e68dabb04da3896dbf0dc5cc05c3332d582b7
Brambs <gabriel128.br@gmail.com>
2024-04-12 10:35:51 -0300

    Transaction page

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from '../../components/Header'
+import Sumarry from '../../components/Summary'
+
+export function index() {
+  return (
+    <div className="">
+      <Header />
+      <Sumarry />
+    </div>
+  )
+}
