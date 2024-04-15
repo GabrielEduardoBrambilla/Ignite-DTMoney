bfda385e17dbe4253c6ee8e1af070374a475b07f
Brambs <gabriel128.br@gmail.com>
2024-04-15 10:12:31 -0300

    Transaction form validation and context implementation

--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -26,7 +26,13 @@ export const SearchFormContainer = styled.form`
     font-weight: bold;
     border-radius: 6px;
     cursor: pointer;
-    &:hover {
+
+    &:disabled {
+      opacity: 0.7;
+      cursor: not-allowed;
+    }
+
+    &:not(:disabled):hover {
       background: ${props => props.theme['green-500']};
       border-color: ${props => props.theme['green-500']};
       color: ${props => props.theme['white']};
