5964e8a400bd7ef5ddb61073f54e967a486e45d2
Brambs <gabriel128.br@gmail.com>
2024-04-15 10:13:06 -0300

    Summary hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,11 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyBtc } from 'phosphor-react'
 import { SummaryContainer, SummaryCard } from './styles'
+import { useSummary } from '../../hooks/useSummary'
+import { priceFormatter } from '../../utils/formatter'
 
 export function Summary() {
+  const summary = useSummary()
+
   return (
     <SummaryContainer>
       <SummaryCard className="">
@@ -9,21 +13,21 @@ export function Summary() {
           <span>Entradas</span>
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
       <SummaryCard className="">
         <header>
           <span>Saídas</span>
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
       <SummaryCard className="" variant="green">
         <header>
           <span>Entradas</span>
           <CurrencyBtc size={32} color="#FFF" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
